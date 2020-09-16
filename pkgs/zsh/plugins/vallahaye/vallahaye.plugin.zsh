90dd61905bc926d3e16d80a5210c29bddce49c6f
Valentin Lahaye <valentin.lahaye@gmail.com>
2020-09-16 16:07:58 +0200

    Add docker and docker-compose aliases (zsh)

--- pkgs/zsh/plugins/vallahaye/vallahaye.plugin.zsh
+++ pkgs/zsh/plugins/vallahaye/vallahaye.plugin.zsh
@@ -6,4 +6,16 @@ alias zln="noglob zmv -L"
 alias zsy="noglob zmv -Ls"
 
 # YouTube-dl
-alias ytdl="youtube-dl -o '%(title)s.%(ext)s' -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0"
+if (( $+commands[youtube-dl] )); then
+  alias ytdl="youtube-dl -o '%(title)s.%(ext)s' -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0"
+fi
+
+# Podman
+if (( $+commands[podman] )); then
+  alias docker="podman"
+fi
+
+# Podman Compose
+if (( $+commands[podman-compose] )); then
+  alias docker-compose="podman-compose"
+fi
